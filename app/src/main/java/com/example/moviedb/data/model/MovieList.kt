69e3d3861460d8eb16a9403341712c7cd6fce368
Emmanuel Ortiz M <oemmanuel617@gmail.com>
2021-04-23 21:25:11 -0400

    add class data

---
+++ app/src/main/java/com/example/moviedb/data/model/MovieList.kt
@@ -0,0 +1,5 @@
+package com.example.moviedb.data.model
+
+data class MovieList(
+        val results: List<Movie> = listOf()
+)
--- app/src/main/java/com/example/moviedb/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/moviedb/data/remote/MovieDataSource.kt
@@ -1,4 +1,18 @@
 package com.example.moviedb.data.remote
 
+import com.example.moviedb.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getTopRatedMovie(): MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovie(): MovieList {
+        return MovieList()
+    }
 }
