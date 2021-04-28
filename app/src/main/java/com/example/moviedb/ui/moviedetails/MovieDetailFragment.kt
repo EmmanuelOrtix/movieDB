cb766d2b31bc4d00e66bdcac6a45c0eb388e1944
Emmanuel Ortiz <emmanuel.ortiz_ex@scotiabank.cl>
2021-04-28 16:41:20 -0400

    finish app

--- app/src/main/java/com/example/moviedb/ui/moviedetails/MovieDetailFragment.kt
+++ app/src/main/java/com/example/moviedb/ui/moviedetails/MovieDetailFragment.kt
@@ -3,16 +3,28 @@ package com.example.moviedb.ui.moviedetails
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.moviedb.R
 import com.example.moviedb.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
 
     private lateinit var binding: FragmentMovieDetailBinding
+    private val args by navArgs<MovieDetailFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMovieDetailBinding.bind(view)
+        Glide.with(requireContext()).load("https://image.tmdb.org/t/p/w500/${args.posterImageUrl}").centerCrop().into(binding.imgMovie)
+        Glide.with(requireContext()).load("https://image.tmdb.org/t/p/w500/${args.posterImageUrl}").centerCrop().into(binding.imgBackground)
+        binding.apply {
+            txtDescription.text = args.overview
+            txtMovieTitle.text = args.title
+            txtLanguage.text = args.language
+            txtRating.text = "${args.voteAverage} (${args.voteCount} Reviews)"
+            txtRelease.text = "Released ${args.releaseDate}"
+        }
     }
 
 }
