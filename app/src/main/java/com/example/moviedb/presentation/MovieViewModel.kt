caa006286a02cf362e8c959024496389356fae21
Emmanuel Ortiz <emmanuel.ortiz_ex@scotiabank.cl>
2021-04-27 20:55:13 -0400

    connect viewmodel an fragment

--- app/src/main/java/com/example/moviedb/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/moviedb/presentation/MovieViewModel.kt
@@ -1,4 +1,47 @@
 package com.example.moviedb.presentation
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.example.moviedb.core.Result
+import com.example.moviedb.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+
+class MovieViewModel(private val repo: MovieRepository) : ViewModel() {
+
+    fun fetchUpcomingMovies() = liveData(Dispatchers.IO) {
+        emit(Result.Loading())
+
+        try {
+            emit(Result.Success(repo.getUpcomingMovies()))
+        } catch (e: Exception) {
+            emit(Result.Failure(e))
+        }
+    }
+
+    fun fetchPopularMovies() = liveData(Dispatchers.IO) {
+        emit(Result.Loading())
+
+        try {
+            emit(Result.Success(repo.getPopularMovies()))
+        } catch (e: Exception) {
+            emit(Result.Failure(e))
+        }
+    }
+
+    fun fetchTopRatedMovies() = liveData(Dispatchers.IO) {
+        emit(Result.Loading())
+
+        try {
+            emit(Result.Success(repo.getTopRatedMovies()))
+        } catch (e: Exception) {
+            emit(Result.Failure(e))
+        }
+    }
+}
+
+class MovieViewModelFactory(private val repo: MovieRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
+    }
 }
--- app/src/main/java/com/example/moviedb/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/example/moviedb/ui/movie/MovieFragment.kt
@@ -1,17 +1,76 @@
 package com.example.moviedb.ui.movie
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.moviedb.R
+import com.example.moviedb.core.Result
+import com.example.moviedb.data.remote.MovieDataSource
 import com.example.moviedb.databinding.FragmentMovieBinding
+import com.example.moviedb.presentation.MovieViewModel
+import com.example.moviedb.presentation.MovieViewModelFactory
+import com.example.moviedb.repository.MovieRepositoryImpl
+import com.example.moviedb.repository.RetrofitClient
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
 
     private lateinit var binding: FragmentMovieBinding
+    private val viewModel by viewModels<MovieViewModel> {
+        MovieViewModelFactory(
+            MovieRepositoryImpl(
+                MovieDataSource(RetrofitClient.webService)
+            )
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMovieBinding.bind(view)
+
+        viewModel.fetchUpcomingMovies().observe(viewLifecycleOwner, Observer { result ->
+            when(result){
+                is Result.Loading -> {
+                    Log.d("Livedata", "Loading")
+                }
+                is Result.Success -> {
+                    Log.d("Livedata", "${result.data}")
+                }
+                is Result.Failure -> {
+                    Log.d("Error", "${result.exception}")
+                }
+            }
+        })
+
+        viewModel.fetchPopularMovies().observe(viewLifecycleOwner, Observer { result ->
+            when(result){
+                is Result.Loading -> {
+                    Log.d("Livedata", "Loading")
+                }
+                is Result.Success -> {
+                    Log.d("Livedata", "${result.data}")
+                }
+                is Result.Failure -> {
+                    Log.d("Error", "${result.exception}")
+                }
+            }
+        })
+
+        viewModel.fetchTopRatedMovies().observe(viewLifecycleOwner, Observer { result ->
+            when(result){
+                is Result.Loading -> {
+                    Log.d("Livedata", "Loading")
+                }
+                is Result.Success -> {
+                    Log.d("Livedata", "${result.data}")
+                }
+                is Result.Failure -> {
+                    Log.d("Error", "${result.exception}")
+                }
+            }
+        })
+
     }
 }
